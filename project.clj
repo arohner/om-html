01ad091fce67f7a8c417d964f48e82e06ba309c5
Stuart Hinson <stuart.hinson@gmail.com>
2017-05-18 11:50:15 -0700

    update clojure v to alpha16, fix spec ns

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/arohner/om-html"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]]
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :creds :gpg}]]
   :jvm-opts ["-Xmx1024m"
