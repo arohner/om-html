086885368686f4c56f52dfd010b1800b273fbe89
Stuart Hinson <stuart.hinson@gmail.com>
2017-10-09 14:35:00 -0400

    Change deps to use provided scope

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://github.com/arohner/om-html"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]]
+  :dependencies [[org.clojure/clojure "1.9.0-beta2" :scope "provided"]
+                 [org.omcljs/om "1.0.0-beta1" :scope "provided"]]
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :creds :gpg}]]
   :jvm-opts ["-Xmx1024m"
